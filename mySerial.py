8f19214f14612c6807e434d2793b031fc2c8b987
Aiace9 <ruggero90@gmail.com>
2015-12-06 16:51:18 +0100

    Documentation

--- mySerial.py
+++ mySerial.py
@@ -3,9 +3,15 @@ __author__ = 'ruggero'
 
 
 class MySerial(serial.Serial):
-    def my_read_line(self):
-        eol = b'\r'
-        leneol = len(eol)
+    def my_read_line(self, eol=b'\r'):
+        """
+        this function read binary data from the serial
+        port up to an eol character or no more character on the line
+        it is a blocking function.
+        :return:
+        return all the characters in binary form
+        """
+        len_eol = len(eol)
         line = bytearray()
         while True:
             if self.inWaiting() == 0:
@@ -13,14 +19,29 @@ class MySerial(serial.Serial):
             c = self.read(1)
             if c:
                 line += c
-                if line[-leneol:] == eol:
+                if line[-len_eol:] == eol:
                     break
             else:
                 break
         return bytes(line)
 
     def inquiring(self, command, rec, save):
+        """
+        this function inquire the serial port with a specific command,
+        wait for the result, and save it in to a csv file.
+        It is useful if it run on a different thread.
+        :param command:
+        command to send
+        :param rec:
+        function to parse the received data
+        :param save:
+        csv writer for saving purpose
+        :return:
+        None
+        """
         self.write(command)
-        print(str(rec(self.my_readl_ine())))
-        save.writerow(["Name", "Address", "Telephone", "Fax", "E-mail", "Others"])
-        # write
+        data = rec(self.my_read_line())
+        print(str(data))
+        data = [i[1] for i in data]
+        save.writerow(data)
+        return None
