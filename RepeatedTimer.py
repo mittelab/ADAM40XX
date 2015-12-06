c5f7ba459407a67142db05999c60e3f3894e3bca
Aiace9 <ruggero90@gmail.com>
2015-12-06 17:00:01 +0100

    removed auto start

--- RepeatedTimer.py
+++ RepeatedTimer.py
@@ -10,7 +10,7 @@ class RepeatedTimer(object):
         self.args = args
         self.kwargs = kwargs
         self.is_running = False
-        self.start()
+        # self.start()
 
     def _run(self):
         self.is_running = False
