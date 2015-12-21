b2fba8b031d4ce8086ab333533309798352a6c0d
Aiace9 <ruggero90@gmail.com>
2015-12-21 16:53:30 +0100

    add serial_ports() method

--- mySerial.py
+++ mySerial.py
@@ -1,4 +1,5 @@
 import serial
+import sys
 __author__ = 'ruggero'
 
 
@@ -45,3 +46,32 @@ class MySerial(serial.Serial):
         data = [i[1] for i in data]
         save.writerow(data)
         return None
+
+    @staticmethod
+    def serial_ports():
+        """ Lists serial port names
+
+            :raises EnvironmentError:
+                On unsupported or unknown platforms
+            :returns:
+                A list of the serial ports available on the system
+        """
+        if sys.platform.startswith('win'):
+            ports = ['COM%s' % (i + 1) for i in range(256)]
+        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+            # this excludes your current terminal "/dev/tty"
+            ports = glob.glob('/dev/tty[A-Za-z]*')
+        elif sys.platform.startswith('darwin'):
+            ports = glob.glob('/dev/tty.*')
+        else:
+            raise EnvironmentError('Unsupported platform')
+
+        result = []
+        for port in ports:
+            try:
+                s = serial.Serial(port)
+                s.close()
+                result.append(port)
+            except (OSError, serial.SerialException):
+                pass
+        return result
